387b030f545b3c0efa2ce464da3c95357ff8478c
Awertol <michal.helgert@seznam.cz>
2022-05-01 16:21:18 +0200

    Přidejte soubory projektu.

---
+++ PrevadecTextu/Prevadec.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrevadecTextu
+{
+    internal class Prevadec
+    {
+        public string PrevadenyText { get; set; }
+
+        public string MalaPismena()
+        {
+            string vysledek = PrevadenyText.ToLower();
+            vysledek = vysledek.Trim();
+            return vysledek;
+        }
+        public string VelkaPismena()
+        {
+           //doplň
+        }
+        public bool ZacinaKonciStejne()
+        {
+            char prvniZnak = PrevadenyText[0];
+            if(PrevadenyText.StartsWith(prvniZnak) && ) //doplň, tohle je jen nápověda :)
+        }
+        public string SpongebobCase()
+        {
+            //vytvoř metodu, která z textu např. "ahoj světe" udělá "aHoJ sVěTe"
+            //nápověda - cyklus + kontrola sudých a lichých písmen :)
+        }
+    }
+}
---
+++ PrevadecTextu/Program.cs
@@ -0,0 +1,16 @@
+﻿using PrevadecTextu;
+
+Prevadec prevadec = new Prevadec();
+
+Console.Write("Zadej text pro převod: ");
+string input = Console.ReadLine();
+
+prevadec.PrevadenyText = input;
+
+//1. 
+Console.WriteLine(prevadec.MalaPismena());
+/*  
+*2. VelkaPismena
+*3. Začíná a končí stejně? ano/ne
+*4. Vypsání Spongebob case 
+*/
